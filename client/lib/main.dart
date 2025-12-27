3094f3c46d93d4dca0871bb0bb5b88674f049763
Falcon <falcon.lin.development@gmail.com>
2025-12-27 15:22:43 -0800

    fix: missing imports

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'screens/home_screen.dart';
